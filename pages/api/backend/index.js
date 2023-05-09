cf5ce5e9a1999042eb45606fdac625636e82e057
Bertrand Riviere <bertrand.riviere@gmail.com>
2023-05-09 16:54:20 +0200

    try to add headers for CORS

--- pages/api/backend/index.js
+++ pages/api/backend/index.js
@@ -1,4 +1,10 @@
 const handler = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,HEAD,PUT,PATCH,POST,DELETE',
+  );
+
   if (req.method === 'GET') {
     res.status(200).json({
       message: 'Hello from protected API',
